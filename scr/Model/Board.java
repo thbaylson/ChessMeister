282fa4ac157ccb7880ff0698039d232756dce840
catupone1 <parafrost77@hotmail.com>
2019-03-01 01:19:00 +0000

    Upload New File

---
+++ scr/Model/Board.java
@@ -0,0 +1,60 @@
+package Model;
+
+import Enums.Files;
+import Enums.Rank;
+import Interfaces.BoardIF;
+import Interfaces.BoardStrategy;
+import Interfaces.PieceIF;
+import Interfaces.SquareIF;
+
+public class Board implements BoardIF{
+
+	@Override
+	public void init_board() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setup() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public SquareIF[][] getSquare() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setDrawStrategy(BoardStrategy d) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public PieceIF getPiece(Rank r, Files f) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
