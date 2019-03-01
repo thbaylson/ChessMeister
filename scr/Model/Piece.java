67995023c9f7137fc96830318cf32b7cadaebfb2
catupone1 <parafrost77@hotmail.com>
2019-03-01 01:19:07 +0000

    Upload New File

---
+++ scr/Model/Piece.java
@@ -0,0 +1,20 @@
+package Model;
+
+import Enums.ChessPieceType;
+import Interfaces.PieceIF;
+
+public class Piece extends BlackAndWhite implements PieceIF{
+
+	@Override
+	public ChessPieceType getChessPieceType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setChessPieceType(ChessPieceType t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
