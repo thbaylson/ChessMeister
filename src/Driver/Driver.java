9e139c27298f33b06d9da530c2184dfc57e38bf0
Dillon Ramsey <dillonramsey5@gmail.com>
2019-03-07 14:30:24 -0500

    Got board to print

--- src/Driver/Driver.java
+++ src/Driver/Driver.java
@@ -1,11 +1,15 @@
 package Driver;
 
 import Model.Board;
+import Interfaces.BoardStrategy;
+import UI_CLI.Board_Mono_CLI;
 
 public class Driver {
 
 	public static void main(String[] args) {
 		Board game = new Board();
+		BoardStrategy gStrat = new Board_Mono_CLI();
+		game.setDrawStrategy(gStrat);
 		game.draw();
 	}
 
