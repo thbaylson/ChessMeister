6a178a782aa88801c831bf9e274cef329e398d33
Caleb <parafrost77@hotmail.com>
2019-04-25 20:54:09 -0400

    Re-made the main menu to use a BorderPane

--- src/GUI/MainMenuScreen.java
+++ src/GUI/MainMenuScreen.java
@@ -6,15 +6,19 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import sun.plugin.javascript.navig.Anchor;
 
 /**
  * @author Caleb Tupone
  */
-public class MainMenuScreen extends VBox implements EventHandler<ActionEvent>{
+public class MainMenuScreen extends BorderPane implements EventHandler<ActionEvent>{
 
     private static MainMenuScreen mainMenuScreen;
+
+    HBox settingsAndExit;
+
+    VBox centerButtonList;
 
     Label title;
 
@@ -23,26 +27,52 @@ public class MainMenuScreen extends VBox implements EventHandler<ActionEvent>{
     Button onlinePlayButton;
     Button rulesButton;
     Button tutorialButton;
+    Button settingsButton;
+    Button exitButton;
 
 
     ScreenChangeHandler sch;
 
     private MainMenuScreen() {
+
+
+        settingsAndExit = new HBox();
+        centerButtonList = new VBox();
+
+
         title = new Label("Chess-Meister");
         title.setScaleX(8);
         title.setScaleY(8);
-        title.setPadding(new Insets(0,0,30,0));
+        title.setPadding(new Insets(25,0,0,0));
 
-        getChildren().add(title);
-        setAlignment(Pos.CENTER);
-        setSpacing(50);
+
+
 
         //Add a button to the root.
-        twoPlayerButton = addButton(this, "Player vs Player");
-        cpuPlayerButton = addButton(this, "Player vs CPU");
-        onlinePlayButton = addButton(this, "Online Play");
-        rulesButton = addButton(this, "Rules of chess");
-        tutorialButton = addButton(this, "Tutorial");
+        twoPlayerButton = addButton(centerButtonList, "Player vs Player");
+        cpuPlayerButton = addButton(centerButtonList, "Player vs CPU");
+        onlinePlayButton = addButton(centerButtonList, "Online Play");
+        rulesButton = addButton(centerButtonList, "Rules of chess");
+        tutorialButton = addButton(centerButtonList, "Tutorial");
+        centerButtonList.setAlignment(Pos.CENTER);
+        centerButtonList.setSpacing(50);
+
+
+        settingsButton = addButton(settingsAndExit, "Settings");
+        exitButton = addButton(settingsAndExit, "Exit");
+        settingsAndExit.setAlignment(Pos.TOP_CENTER);
+        settingsAndExit.setSpacing(200);
+        settingsAndExit.setPadding(new Insets(0, 0, 50, 0));
+
+
+
+        setTop(title);
+        setAlignment(title, Pos.CENTER);
+        setCenter(centerButtonList);
+        setBottom(settingsAndExit);
+
+
+
 
 
 
