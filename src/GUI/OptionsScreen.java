cb2137ec4dcab5a95aeadfcfe391bf50044f5e48
Dillon Ramsey <dillonramsey5@gmail.com>
2019-05-03 23:07:22 -0400

    Merged with caleb

--- src/GUI/OptionsScreen.java
+++ src/GUI/OptionsScreen.java
@@ -5,6 +5,7 @@ import Interfaces.ScreenChangeHandler;
 import Interfaces.SquareColorHandler;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
--- src/GUI/PlayerPane.java
+++ src/GUI/PlayerPane.java
@@ -1,48 +1,140 @@
 package GUI;
 
+import Enums.GameColor;
+import Interfaces.PieceIF;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
+import java.util.ArrayList;
 
+
+/**
+ * Objects representing each players captured pieces
+ *
+ * @author Tyler 60% PlayerPane
+ * @author Dillon 40% addChilden getPlayer
+ */
 public class PlayerPane extends VBox {
 
-    private Label playerName;
+    /**The player*/
+    private int player;
+    /**Tile pane containing captured pieces*/
+    private TilePane capturedPieces;
+    /**Array of images*/
+    private ArrayList<ImageView> iArray;
 
+    /**
+     * Constructor for  PlayerPane
+     * @param playerNumber - The player number
+     */
     public PlayerPane(int playerNumber){
+        this.player = playerNumber;
+        iArray = new ArrayList<>();
         setPrefSize(200, 400);
         setSpacing(40);
         Label title = new Label("Player " + playerNumber);
         title.getStyleClass().add("my-label");
         title.setPadding(new Insets(20,0,0,0));
-        title.setScaleX(1.5);
-        title.setScaleY(1.5);
+        title.setScaleX(1.2);
+        title.setScaleY(1.2);
 
-        if (playerNumber == 1){
-            playerName = new Label(PlayerNameScreen.getInstance().getPlayerOneField());
-        }
-        else {
-            playerName = new Label(PlayerNameScreen.getInstance().getPlayerTwoField());
-        }
-
+        Label playerName = new Label("P" + playerNumber);
         playerName.getStyleClass().add("my-label");
         playerName.setPadding(new Insets(0,0,40,0));
         playerName.setAlignment(Pos.CENTER_RIGHT);
-        playerName.setScaleX(1.5);
-        playerName.setScaleY(1.5);
+        playerName.setScaleX(1.2);
+        playerName.setScaleY(1.2);
 
         Label capturedLabel = new Label("Captured:");
         capturedLabel.getStyleClass().add("my-label");
-        capturedLabel.setScaleX(1.5);
-        capturedLabel.setScaleY(1.5);
+        capturedLabel.setScaleX(1.2);
+        capturedLabel.setScaleY(1.2);
 
-        HBox capturedPieces = new HBox();
+        capturedPieces = new TilePane();
+        capturedPieces.setPrefColumns(2);
+        capturedPieces.setMaxHeight(30);
+        capturedPieces.setMinWidth(10);
 
         getChildren().addAll(
                 title,
                 playerName,
                 capturedLabel,
                 capturedPieces);
+    }
+
+    /**
+     * Adds pieces to the captured area
+     * @param pieces - The pieces to add
+     */
+    public void addChilden(ArrayList<PieceIF> pieces){
+        capturedPieces.getChildren().removeAll(iArray);
+        iArray = new ArrayList<>();
+        for (PieceIF piece : pieces){
+            ImageView image = null;
+            if (piece != null) {
+                switch (piece.getChessPieceType()) {
+                    case King:
+                        if (piece.getColor() == GameColor.WHITE) {
+                            image = new ImageView(new Image(getClass().getResourceAsStream("./images/WK.png")));
+                        } else {
+                            image = new ImageView(new Image(getClass().getResourceAsStream("./images/BK.png")));
+                        }
+                        break;
+                    case Queen:
+                        if (piece.getColor() == GameColor.WHITE) {
+                            image = new ImageView(new Image(getClass().getResourceAsStream("./images/WQ.png")));
+                        } else {
+                            image = new ImageView(new Image(getClass().getResourceAsStream("./images/BQ.png")));
+                        }
+                        break;
+                    case Rook:
+                        if (piece.getColor() == GameColor.WHITE) {
+                            image = new ImageView(new Image(getClass().getResourceAsStream("./images/WR.png")));
+                        } else {
+                            image = new ImageView(new Image(getClass().getResourceAsStream("./images/BR.png")));
+                        }
+                        break;
+                    case Knight:
+                        if (piece.getColor() == GameColor.WHITE) {
+                            image = new ImageView(new Image(getClass().getResourceAsStream("./images/WN.png")));
+                        } else {
+                            image = new ImageView(new Image(getClass().getResourceAsStream("./images/BN.png")));
+                        }
+                        break;
+                    case Bishop:
+                        if (piece.getColor() == GameColor.WHITE) {
+                            image = new ImageView(new Image(getClass().getResourceAsStream("./images/WB.png")));
+                        } else {
+                            image = new ImageView(new Image(getClass().getResourceAsStream("./images/BB.png")));
+                        }
+                        break;
+                    case Pawn:
+                        if (piece.getColor() == GameColor.WHITE) {
+                            image = new ImageView(new Image(getClass().getResourceAsStream("./images/WP.png")));
+                        } else {
+                            image = new ImageView(new Image(getClass().getResourceAsStream("./images/BP.png")));
+                        }
+                        break;
+                }
+            }
+            if (image != null) {
+                image.setFitHeight(capturedPieces.getMaxHeight());
+                image.setFitWidth(capturedPieces.getMaxWidth());
+                iArray.add(image);
+            }
+        }
+        capturedPieces.getChildren().addAll(iArray);
+    }
+
+    /**
+     * Gets the player number
+     * @return The player's number
+     */
+    public int getPlayer(){
+        return player;
     }
 }
